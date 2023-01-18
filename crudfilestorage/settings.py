4453d6ad18b28c3d381be9574e9c56d51331b406
Evg <macheev2010@yandex.ru>
2023-01-19 01:43:07 +0300

    Use SessionAuthentication

--- crudfilestorage/settings.py
+++ crudfilestorage/settings.py
@@ -122,7 +122,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
