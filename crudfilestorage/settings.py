a2cb1c60f86e913d99b05d1408e106eb0e3b119f
Evg <macheev2010@yandex.ru>
2023-01-19 00:31:50 +0300

    Add basic authentication support

--- crudfilestorage/settings.py
+++ crudfilestorage/settings.py
@@ -122,6 +122,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
