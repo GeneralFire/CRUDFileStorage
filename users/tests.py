e0a68dedbc132bc6a0ad4c66f0bb6720b4ecf588
Evg <macheev2010@yandex.ru>
2023-01-19 03:28:22 +0300

    Add profile creation tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+from .models import User, Profile
 
 # Create your tests here.
+
+
+class UserNewUserTest(TestCase):
+
+    USERNAME = 'USERNAME'
+    PASSWORD = '123'
+
+    def setUp(set):
+        User.objects.create(
+            username=UserNewUserTest.USERNAME,
+            password=UserNewUserTest.PASSWORD
+        )
+
+    def test_correponding_profile(self):
+        user = User.objects.get(
+            username=UserNewUserTest.USERNAME
+        )
+        profile = Profile.objects.get(user=user)
+        self.assertEqual(
+            profile.user, user
+        )
+
+
+class UserDeleteUserTest(TestCase):
+
+    USERNAME = 'USERNAME'
+    PASSWORD = '123'
+
+    def setUp(set):
+        User.objects.create(
+            username=UserDeleteUserTest.USERNAME,
+            password=UserDeleteUserTest.PASSWORD
+        )
+
+    def test_correponding_profile(self):
+        user = User.objects.get(
+            username=UserDeleteUserTest.USERNAME
+        )
+        user.delete()
+        with self.assertRaises(
+            Profile.DoesNotExist,
+        ):
+            Profile.objects.get(user=user)
